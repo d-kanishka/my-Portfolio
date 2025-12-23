abac57ac6e89fbd5e36d5d19fd772751bff57222
kanishka_d <kanishkad1210@gmail.com>
2025-12-23 09:55:28 +0530

    changed default mode as dark mode

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
 
   useEffect(() => {
     const stored = localStorage.getItem("theme") as "light" | "dark" | null;
